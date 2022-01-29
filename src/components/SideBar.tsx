26cc3e05354d894b6e25a84175bd790fba213cc8
Juliano Alves <contato.julianoalvess@gmail.com>
2022-01-29 18:33:42 -0300

    feat: 🎸 component update
    component update

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,38 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+import { GenreResponseProps } from "./../domain/models";
+
+interface SidebarProps {
+  handleClickButton: (id: number) => void;
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+}
+
+export function SideBar({
+  handleClickButton,
+  genres,
+  selectedGenreId,
+}: SidebarProps) {
+  return (
+    <>
+      <nav className="sidebar">
+        <span>
+          Watch<p>Me</p>
+        </span>
+
+        <div className="buttons-container">
+          {genres?.map((genre) => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+      </nav>
+      ;
+    </>
+  );
 }
